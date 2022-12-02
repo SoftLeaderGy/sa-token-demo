84fbf96f85d4955e30499b08bb8f0862b465bc18
SoftLeaderGy <>
2022-12-02 16:54:24 +0800

    集成jwt、集成redis token使用jwt、测试token存的字段值

--- src/main/java/com/yang/controller/LoginController.java
+++ src/main/java/com/yang/controller/LoginController.java
@@ -1,11 +1,16 @@
 package com.yang.controller;
 
+import cn.dev33.satoken.stp.SaLoginConfig;
 import cn.dev33.satoken.stp.SaTokenInfo;
 import cn.dev33.satoken.stp.StpUtil;
 import cn.dev33.satoken.util.SaResult;
+import com.alibaba.fastjson.JSONObject;
+import com.yang.config.SaTokenConfigure;
 import com.yomahub.tlog.core.annotation.TLogAspect;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @Description: 登录相关控制器
@@ -18,13 +23,14 @@ public class LoginController {
 
     @RequestMapping("/doLogin")
     @TLogAspect(str = "XYZ")
-    public String doLogin(String username, String password) {
+    public SaResult doLogin(String username, String password) {
         // 此处仅作模拟示例，真实项目需要从数据库中查询数据进行比对
         if("zhang".equals(username) && "123456".equals(password)) {
-            StpUtil.login(10001);
-            return "登录成功";
+            StpUtil.login(10001, SaLoginConfig.setExtra("name","zhang"));
+            SaTokenInfo tokenInfo = StpUtil.getTokenInfo();
+            return SaResult.data(tokenInfo);
         }
-        return "登录失败";
+        return SaResult.error();
     }
 
     // 查询登录状态，浏览器访问： http://localhost:8081/user/isLogin
@@ -33,19 +39,11 @@ public class LoginController {
         return "当前会话是否登录：" + StpUtil.isLogin();
     }
 
-    /**
-     * 返回token
-     * @param username
-     * @param password
-     * @return
-     */
-    @RequestMapping("/getTokenInfo")
-    public SaResult getTokenInfo(String username, String password){
-        if("gy".equals(username) && "123".equals(password)){
-            StpUtil.login(10001);
-            SaTokenInfo tokenInfo = StpUtil.getTokenInfo();
-            return SaResult.data(tokenInfo);
-        }
-        return SaResult.error("获取token失败");
+    @RequestMapping("/getTokenExtra")
+    public String getTokenExtra(HttpServletRequest request){
+        String accessToken = request.getHeader("accessToken");
+        Object name = StpUtil.getExtra(accessToken, "name");
+        Object loginId = StpUtil.getLoginIdByToken(accessToken);
+        return "当前登录的姓名为====> " + name + " " + JSONObject.toJSONString(loginId);
     }
 }
